4668504bce7971dfeb6b2b8adce1849192c407f9
Aditya24 <aadityabante@gmail.com>
2020-09-11 21:50:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Todo from "./components/Todo.js";
+//rafce :- create arrow function
+function App() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
